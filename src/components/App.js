ba4abe442580defe7df6feae9b392c7255ac7a52
abhisheksonkar20 <sonkar.abhishek45@gmail.com>
2020-11-19 00:52:34 +0530

    updated code

--- src/components/App.js
+++ src/components/App.js
@@ -2,12 +2,15 @@ import React, {Component, useState} from "react";
 import "./../styles/App.css";
 
 function App() {
+  const [paraShown, setParaShown] = useState(false);
   return (
     <div id="main">
-      // Do not alter the main div
+      <button id="click" onClick={() => setParaShown(true)}>Click Me</button>
+				{paraShown ? (
+					<p id="para">Hello, I've learnt to use the full-stack evaluation tool. This makes me so happy</p>
+				) : null}
     </div>
   );
 }
-
 
 export default App;
